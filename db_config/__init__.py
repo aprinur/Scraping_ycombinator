21a0597b8e06683666c9f0b82de6e947e87f945c
Apri Nur <aprinur7@gmail.com>
2025-01-13 14:45:02 +0700

    add default database path

--- db_config/__init__.py
+++ db_config/__init__.py
@@ -1,9 +1,34 @@
+import tempfile
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, scoped_session, declarative_base
 from selenium import webdriver
+import os
+from pathlib import Path
 
 
-DB_URL = f'sqlite:///D:/Github/aprinur/scrape_ycombinator.com/ycombinator.db'
+def get_db_path():
+    try:
+        current_dir = os.path.dirname(os.path.abspath(__file__))
+        db_path = os.path.join(current_dir, 'YCombinator.db')
+        Path(db_path).touch()
+        print(f'Database will be saved in: {db_path}')
+        return f'sqlite:///{db_path}'
+    except:
+        try:
+            home_dir = str(Path.home())
+            db_path = os.path.join(home_dir, "YCombinator.db")
+            Path(db_path).touch()
+            print(f'Database will be saved in: {db_path}')
+            return f"slite:///{db_path}"
+        except:
+            temp_dir = tempfile.gettempdir()
+            db_path = os.path.join(temp_dir, 'YCombinator.db')
+            print(f'Database will be saved in: {db_path}')
+            return f'sqlite:///{db_path}'
+
+
+DB_URL = get_db_path()
 engine = create_engine(DB_URL, echo=False)
 Base = declarative_base()
 
